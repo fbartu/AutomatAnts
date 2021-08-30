b9c151792050b4583daf4570d243b3fefdd6384a
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2021-08-30 16:59:56 +0200

    Created bash script for recruitment experiments

---
+++ code/SerialVsParallel_recruitment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+# normal model
+python3 ~/research/AutomatAnts/code/run_model.python
+# scanning parameter mu (noise in recruitment)
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.01
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.05
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.1
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.15
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.20
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.25
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.30
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.35
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.40
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.50
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=0.75
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=1
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=2
+python3 ~/research/AutomatAnts/code/run_model.python -p mu=3
+# serial recruitment
+python3 ~/research/AutomatAnts/code/run_model.python -r HR,s 
+deactivate
+
+#how to run
+#qsub ~/research/AutomatAnts/run_model.sh 
+#qsub -pe make 1 -l h_vmem=8G ~/research/AutomatAnts/run_model.sh
