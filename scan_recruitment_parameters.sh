206912681f30799a30912a8cc2c3de0f407d9893
Polfe94 <pfernandez@ceab.csic.es>
2021-12-21 11:53:56 +0100

    Addition of recruitment parameter scan

---
+++ scan_recruitment_parameters.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# root directory
+cd
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+
+# default model
+python3 ~/research/AutomatAnts/code/run_model.py -x Control_Noise -r GR --nruns 20,True
+
+# scanning noise in recruitment
+for i in $(seq 0.001 0.2 10)
+do 
+echo "Running model with gamma_1 = $i"
+python3 ~/research/AutomatAnts/code/run_model.py -p gamma_1=$i -x gamma_1$i --nruns 20,True 
+echo "Running model with gamma_2 = $i"
+python3 ~/research/AutomatAnts/code/run_model.py -p gamma_2=$i -x gamma_2$i --nruns 20,True 
+echo "Running model with both gammas = $i"
+python3 ~/research/AutomatAnts/code/run_model.py -p gamma_1=$i,gamma_2=$i -x gamma_1and2$i --nruns 20,True 
+done
+
+deactivate
+
+#how to run
+#qsub ~/research/AutomatAnts/code/run_model.sh 
+#qsub -pe make 20 -l h_vmem=8G ~/research/AutomatAnts/code/run_model.sh
