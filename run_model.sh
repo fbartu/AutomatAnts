13e66005f6ff4099342abe8624c9cabe2750ab01
Polfe94 <pfernandez@ceab.csic.es>
2021-12-22 13:40:11 +0100

    Running default model

--- run_model.sh
+++ run_model.sh
@@ -4,7 +4,8 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-python3 ~/research/AutomatAnts/code/run_model_time.py
+#python3 ~/research/AutomatAnts/code/run_model_time.py
+python3 ~/research/AutomatAnts/code/run_model.py -x Control_Noise -r GR --nruns 20,True
 deactivate
 
 #how to run
