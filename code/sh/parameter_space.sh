dedf7fa355bfc11618af37eba1a3b10d57e47679
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-08-02 16:09:42 +0200

    Added bash files to automate simulations

--- code/sh/parameter_space.sh
+++ code/sh/parameter_space.sh
@@ -10,9 +10,9 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
 
-for i in $(seq 0.0004 0.0004 0.012)
+for alpha in $(seq 0.0004 0.0004 0.012)
 do
-    for j in $(seq 0.05 0.1 3)
+    for beta in $(seq 0.05 0.1 3)
     do
     python3 ~/research/AutomatAnts/code/Run.py --directory ~/research/AutomatAnts/results/parameter_space/ --filename "alpha=${alpha/,/.}_beta=${beta/,/.}" -n 100 -p "alpha=${alpha/,/.},beta=${beta/,/.}"
     # python3 ~/research/2022/ANTS/AutomatAnts/code/Run.py --directory ~/research/2022/ANTS/AutomatAnts/results/parameter_space/ --filename "alpha=${alpha/,/.}_beta=${beta/,/.}" -n 1 -p "alpha=${alpha/,/.},beta=${beta/,/.}"
