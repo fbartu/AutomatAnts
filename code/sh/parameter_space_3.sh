69b3034b3e076d9184fd61e049ade0117cebe415
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-08-07 10:57:08 +0200

    dividing parameter space scanning

--- code/sh/parameter_space_3.sh
+++ code/sh/parameter_space_3.sh
@@ -15,7 +15,7 @@ do
     # for beta in $(seq 0.05 0.1 3)
     for beta in $(seq 0.05 0.2 3)
     do
-    # python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/parameter_space/ --filename alpha=${alpha/,/.}_beta=${beta/,/.} -n 100 -p alpha=${alpha/,/.},beta=${beta/,/.}
+    python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/parameter_space/ --filename alpha=${alpha/,/.}_beta=${beta/,/.} -n 100 -p alpha=${alpha/,/.},beta=${beta/,/.}
     # python3 ~/research/2022/ANTS/AutomatAnts/code/run_cluster.py --directory ~/research/2022/ANTS/AutomatAnts/results/parameter_space/ --filename alpha=${alpha/,/.}_beta=${beta/,/.} -n 100 -p alpha=${alpha/,/.},beta=${beta/,/.}
     done
 done
