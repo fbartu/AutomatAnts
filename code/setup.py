ac34abc745674e7d09cef54fb44cf14bf60462cd
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-09-07 12:21:01 +0200

    removed some unnecessary scripts

--- code/setup.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import distutils.core
-
-sys.path.append('/home/polfer/.local/lib/python3.8/site-packages/')
-
-import Cython.Build
-distutils.core.setup(
-    ext_modules = Cython.Build.cythonize("Run.pyx"))
