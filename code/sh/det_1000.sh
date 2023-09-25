87587530204535b3bdbbfa0e0665c8a201d0aa93
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-09-25 16:47:49 +0200

    corrected error in det_1000.sh

--- code/sh/det_1000.sh
+++ code/sh/det_1000.sh
@@ -10,6 +10,6 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-python3 ~/research/AutomatAnts/code/Run.py --directory ~/research/AutomatAnts/results/food_conditions/det/ --food det --filename det -n 1000
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/food_conditions/det/ --food det --filename det -n 1000
 deactivate
 
