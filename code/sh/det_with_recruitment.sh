a3194168d4cdc636d10da669f2ca6a6b04be86ed
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-10-02 11:55:55 +0200

    added sh to check the recruitment mechanics

---
+++ code/sh/det_with_recruitment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#how to run
+#qsub ~/research/AutomatAnts/code/run_model.sh 
+#qsub -pe make 20 -l h_vmem=8G ~/research/AutomatAnts/code/run_model.sh
+
+cd
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/det_with_recruitment --food det --filename det_rec -n 100
+deactivate
+
