9db209d5b0749af49ab11947d24053679fbb2ea8
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-10-17 16:07:58 +0200

    added a global parameter space scan

--- code/sh/parameter_space.sh
+++ code/sh/parameter_space.sh
@@ -10,7 +10,7 @@ for alpha in $(seq 0.00001 0.00005 0.0075)
 do
     for beta in $(seq 0.05 0.1 3)
     do
-    python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/parameter_space/ --filename alpha=${alpha/,/.}_beta=${beta/,/.} -n 100 -p alpha=${alpha/,/.},beta=${beta/,/.}
+    python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameter_space/ --filename alpha=${alpha/,/.}_beta=${beta/,/.} -n 100 -p alpha=${alpha/,/.},beta=${beta/,/.}
     done
 done
 deactivate
