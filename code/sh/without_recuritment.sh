419c3545eb1643ac726cb086773ace33602e07b4
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-11-10 16:11:47 +0100

    added sh script to run simulations without recruitment

---
+++ code/sh/without_recuritment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#how to run
+#qsub ~/research/AutomatAnts/code/run_cluster.sh 
+
+cd
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/without_recruitment/parameters/uniform/ --food det --filename "uniform" -n 100 
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/without_recruitment/parameters/gaussian/ --food det --filename "gaussian" -n 100 -g 100,B,2,2
+
+deactivate
