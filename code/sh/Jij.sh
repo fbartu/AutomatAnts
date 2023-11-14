1a0eecaa30fdaf11188fefb77776d5fede7ad061
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-11-14 10:55:39 +0100

    corrected bug when parsing Jij

--- code/sh/Jij.sh
+++ code/sh/Jij.sh
@@ -10,7 +10,7 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
 # Jij = 1
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/homogeneous_Jij/Jij_1/uniform/ --food det --filename "Jij1_uniform" -n 100 -p 'Jij={"0-0":1,"0-1":1,"1-0":1,"1-1":1}"'
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/homogeneous_Jij/Jij_1/uniform/ --food det --filename "Jij1_uniform" -n 100 -p "Jij={\'0-0':1,\'0-1':1,\'1-0':1,\'1-1':1}"
 python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/homogeneous_Jij/Jij_1/gaussian/ --food det --filename "Jij1_gaussian" -n 100 -g 100,B,2,2 -p 'Jij={"0-0":1,"0-1":1,"1-0":1,"1-1":1}"'
 python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/homogeneous_Jij/Jij_1/bimodal_50/ --food det --filename "Jij1_bimodal_50" -n 100 -g 50,B,2,6:50,B,6,2 -p 'Jij={"0-0":1,"0-1":1,"1-0":1,"1-1":1}"'
 python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/homogeneous_Jij/Jij_1/bimodal_90/ --food det --filename "Jij1_bimodal_90" -n 100 -g 90,B,2,6:10,B,6,2 -p 'Jij={"0-0":1,"0-1":1,"1-0":1,"1-1":1}"'
