93a31d1cfaafbc4f8fce998045583b7d3e2c16ea
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2023-11-14 09:15:20 +0100

    corrected a bug; setting recruitment rules properly

--- code/sh/bimodal_gains.sh
+++ code/sh/bimodal_gains.sh
@@ -9,6 +9,7 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/uniform/ --food det --filename "uniform" -n 100
 python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/bimodal/ --food det --filename "bimodal" -n 100 -g 100,B,0.5,0.5
 python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/gaussian/ --food det --filename "gaussian" -n 100 -g 100,B,2,2
 python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/with_recruitment/parameters/bimodal_50/ --food det --filename "bimodal_50" -n 100 -g 50,B,2,6:50,B,6,2
--- code/sh/without_recuritment.sh
+++ code/sh/without_recuritment.sh
@@ -9,7 +9,7 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/without_recruitment/parameters/uniform/ --food det --filename "uniform" -n 100 
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/without_recruitment/parameters/gaussian/ --food det --filename "gaussian" -n 100 -g 100,B,2,2
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/without_recruitment/parameters/uniform/ --food det --filename "uniform" -n 100 -r False
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/without_recruitment/parameters/gaussian/ --food det --filename "gaussian" -n 100 -g 100,B,2,2 -r False
 
 deactivate
