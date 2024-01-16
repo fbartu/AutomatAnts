c5c5cdb26eaf1d1a84a18af656aa83cecaaf35cd
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-01-16 09:47:02 +0100

    added error check output in default.sh

--- code/sh/default.sh
+++ code/sh/default.sh
@@ -9,6 +9,6 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/default/ --filename default -n 100
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/default/ --filename default -n 100 > errorchk.txt
 
 # deactivate
