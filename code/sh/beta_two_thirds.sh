1122cf9f5af68b56ed3d969b8fde1af3d6be878c
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-01-16 13:40:11 +0100

    changing betatwothirds

--- code/sh/beta_two_thirds.sh
+++ code/sh/beta_two_thirds.sh
@@ -9,8 +9,8 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-beta=1
-# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_1/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_1/ --filename beta=1 -n 100 -p beta=1
+beta=0.66
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.66/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
+# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_1/ --filename beta=1 -n 100 -p beta=1
 
 # deactivate
