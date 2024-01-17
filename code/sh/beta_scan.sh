ced61a67f3cfc8584a1b93085f3b0566a04eee99
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-01-17 15:20:22 +0100

    parametrizatiooooon

--- code/sh/beta_scan.sh
+++ code/sh/beta_scan.sh
@@ -9,11 +9,13 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-beta=0.7
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.7/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
-beta=0.8
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.8/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
-beta=0.9
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.9/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
+beta=0.5
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.5/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
+# beta=0.7
+# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.7/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
+# beta=0.8
+# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.8/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
+# beta=0.9
+# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/beta_0.9/ --filename beta=${beta/,/.} -n 100 -p beta=${beta/,/.}
 
 # deactivate
