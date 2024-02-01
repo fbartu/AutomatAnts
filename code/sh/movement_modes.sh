93bfb982f3c039a72e5d1e03c048ade104b7889d
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-01 11:11:15 +0100

    reverting back to the original motility matrix

--- code/sh/movement_modes.sh
+++ code/sh/movement_modes.sh
@@ -9,8 +9,8 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/nest_avoid/ --filename ballistic -n 100 -m bal
-# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/random/ --filename random -n 100 -m random
-python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/modified_mot/ --filename modified_mot -n 100 -m exp
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/nest_avoid/ --filename ballistic -n 100 -m bal
+python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/random/ --filename random -n 100 -m random
+# python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/updated_params/modified_mot/ --filename modified_mot -n 100 -m exp
 
 # deactivate
