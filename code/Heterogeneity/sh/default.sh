c7d80cb394ddedbb962176e66547cc92aef349fd
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-12 10:19:12 +0100

    adding default simulations with the corresponding corrections

---
+++ code/Heterogeneity/sh/default.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#how to run
+#qsub ~/research/AutomatAnts/code/run_model.sh 
+
+cd
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/default/ --filename default -n 100 -m exp
