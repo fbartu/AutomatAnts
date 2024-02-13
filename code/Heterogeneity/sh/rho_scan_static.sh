5aa2885c64d998f5315183db9daab6412fac4a1e
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-13 10:17:46 +0100

    corrected path bug

--- code/Heterogeneity/sh/rho_scan_static.sh
+++ code/Heterogeneity/sh/rho_scan_static.sh
@@ -11,5 +11,5 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 . ~/research/automatenv/bin/activate
 for rho in $(seq 0.001 0.05 1.001)
 do
-    python3 ~/research/AutomatAnts/code/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/static/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
+    python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/static/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
 done
