281bff8f7e6284c0099fd3a8d39c92ec355d4940
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-20 12:51:29 +0100

    changed to NFD mode

--- code/Heterogeneity/sh/rho_scan_shutdown.sh
+++ code/Heterogeneity/sh/rho_scan_shutdown.sh
@@ -11,6 +11,6 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 . ~/research/automatenv/bin/activate
 for rho in $(seq 0.001 0.05 1.001)
 do
-    python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/DET/shutdown/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
-    # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/NFD/shutdown/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
+    # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/DET/shutdown/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
+    python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/NFD/shutdown/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
 done
