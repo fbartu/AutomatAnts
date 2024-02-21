fe9c35df1c27eb5b4cd74e2623a10d3253acb1d3
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-21 17:00:42 +0100

    proportions of no listeners for LR

--- code/Heterogeneity/sh/rho_no_listen.sh
+++ code/Heterogeneity/sh/rho_no_listen.sh
@@ -9,9 +9,9 @@ export PATH=/home/soft/python-3.9.5/bin:$PATH
 export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
-for rho in $(seq 0.001 0.05 1.001)
+for rho in $(seq 0.001 0.1 1.001)
 do
-    for epsilon in $(seq 0.001 0.05 1.001)
+    for epsilon in $(seq 0.001 0.1 1.001)
     do
         python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/LR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
         # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/SR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
