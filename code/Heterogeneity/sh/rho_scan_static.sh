17d3defc5b0e7d42077e7daf50e1ed6ab0ec6407
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-21 11:57:32 +0100

    changed rho scan to probability of recruitment

--- code/Heterogeneity/sh/rho_scan_static.sh
+++ code/Heterogeneity/sh/rho_scan_static.sh
@@ -12,5 +12,7 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 for rho in $(seq 0.001 0.05 1.001)
 do
     # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/DET/static/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
-    python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/NFD/static/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
+    # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/NFD/static/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
+    python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/prob_rec/ --filename rho_${rho/,/.} -n 100 -p rho=${rho/,/.}
+
 done
