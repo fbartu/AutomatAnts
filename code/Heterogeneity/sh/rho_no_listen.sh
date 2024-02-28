3f52e70c123b7ad5006245a2b8e8a8c3423e1328
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-28 15:38:19 +0100

    LR no social feedbacks

--- code/Heterogeneity/sh/rho_no_listen.sh
+++ code/Heterogeneity/sh/rho_no_listen.sh
@@ -13,8 +13,8 @@ for rho in $(seq 0.001 0.1 1.001)
 do
     for epsilon in $(seq 0.001 0.1 1.001)
     do
-        # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/LR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/LR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
         # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/SR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
-        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/both_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+        # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/both_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
     done
 done
