9481a921049f5d314bf4e7e078f927649298714d
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-02-29 17:19:15 +0100

    LR no listen

--- code/Heterogeneity/sh/rho_no_listen.sh
+++ code/Heterogeneity/sh/rho_no_listen.sh
@@ -14,8 +14,8 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 rho=0
     for epsilon in $(seq 0.001 0.1 1.001)
     do
-        # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/LR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
-        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/SR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/LR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+        # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/SR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
         # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/both_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
     done
 # done
