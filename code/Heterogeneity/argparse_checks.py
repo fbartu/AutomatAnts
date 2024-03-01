ad9e81bac672a041ac602c6a466edac5be38f5d3
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-03-01 16:05:15 +0100

    bug correction in feedbacks

--- code/Heterogeneity/argparse_checks.py
+++ code/Heterogeneity/argparse_checks.py
@@ -4,63 +4,77 @@ from Model import *
 Jij = {'0-0': 1,'1-1': 1,'1-0': 1,'0-1': 1}
 
 def argparser(argv = sys.argv[1:]):
-	
-	opts, args = getopt.getopt(argv, 'n:d:x:f:m:p:g:r:',
-							['nruns=', 'directory=', 'filename=', 
-								'food=', 'movement=', 'parameters=',
-							'gains=', 'recruitment='])
+        
+    opts, args = getopt.getopt(argv, 'n:d:x:f:m:p:g:r:s:',
+                            ['nruns=', 'directory=', 'filename=', 
+                                'food=', 'movement=', 'parameters=',
+                            'gains=', 'recruitment=', 'social_feedbacks='])
 
-	parameters = {}
+    parameters = {'filename': 'simulation',
+               'runs': 1, 'results_path': '../results/',
+               'food_condition': 'det',
+               'alpha': alpha, 'beta': beta, 
+                  'gamma': gamma, 'Jij': Jij,
+                 'Theta': Theta}
 
-	for opt, arg in opts:
-		if opt in ('-n', '--nruns'):
-			parameters['runs'] = int(arg)
-			
-		elif opt in ('-d', '--directory'):
-			parameters['results_path'] = arg
-			
-		elif opt in ('-x', '--filename'):
-			parameters['filename'] = arg
-			
-		elif opt in ('-f', '--food'):
-			splarg = arg.split(',')
-			parameters['food_condition'] = splarg[0]
-			if len(splarg) == 2:
-				parameters['d'] = splarg[1]
-			
-		elif opt in ('-m', '--movement'):
-			parameters['default_movement'] = arg
+    for opt, arg in opts:
+        if opt in ('-n', '--nruns'):
+            parameters['runs'] = int(arg)
+            
+        elif opt in ('-d', '--directory'):
+            parameters['results_path'] = arg
+            
+        elif opt in ('-x', '--filename'):
+            parameters['filename'] = arg
+            
+        elif opt in ('-f', '--food'):
+            splarg = arg.split(',')
+            parameters['food_condition'] = splarg[0]
+            if len(splarg) == 2:
+                parameters['d'] = splarg[1]
+            
+        elif opt in ('-m', '--movement'):
+            parameters['default_movement'] = arg
    
-		elif opt in ('-r', '--recruitment'):
-			parameters['recruitment'] = eval(arg)
-	   
-		elif opt in ('-p', '--parameters'):
-			plist = arg.split(';')
-			for p in plist:
-				x = p.split('=')
-				if x[0] == 'alpha':
-					parameters['alpha'] = eval(x[1])
-				elif x[0] == 'beta':
-					parameters['beta'] = eval(x[1])
-				elif x[0] == 'gamma':
-					parameters['gamma'] = eval(x[1])
-				elif x[0] == 'Jij':
-					j = eval(x[1])
-					if type(j) == dict:
-						if j.keys() == Jij.keys():
-							parameters['Jij'] = j
-				elif x[0] == 'Theta':
-					parameters['Theta'] = eval(x[1])
-				elif x[0] == 'g':
-					parameters['g'] = arg
-				elif x[0] == 'rho':
-					parameters['rho'] = eval(x[1])
-				else:
-					print('Unknown parameter', x[0], flush= True)
-		elif opt in ('-g', '--gains'):
-			parameters['g'] = arg
-			
-	return parameters
+        elif opt in ('-r', '--recruitment'):
+            parameters['recruitment'] = eval(arg)
+   
+        if opt in ('-s', '--social_feedbacks'):
+                parameters['feedback'] = arg
+       
+        elif opt in ('-p', '--parameters'):
+            plist = arg.split(';')
+            for p in plist:
+                x = p.split('=')
+                if x[0] == 'alpha':
+                    parameters['alpha'] = eval(x[1])
+                elif x[0] == 'beta':
+                    parameters['beta'] = eval(x[1])
+                elif x[0] == 'gamma':
+                    parameters['gamma'] = eval(x[1])
+                elif x[0] == 'Jij':
+                    j = eval(x[1])
+                    if type(j) == dict:
+                        if j.keys() == Jij.keys():
+                            parameters['Jij'] = j
+                elif x[0] == 'Theta':
+                    parameters['Theta'] = eval(x[1])
+                elif x[0] == 'g':
+                    parameters['g'] = arg
+                elif x[0] == 'rho':
+                    parameters['rho'] = eval(x[1])
+                elif x[0] == 'epsilon':
+                    parameters['epsilon'] = eval(x[1])
+                else:
+                    print('Unknown parameter', x[0], flush= True)
+                    try:
+                        parameters[x[0]] = eval(x[1])
+                    except:
+                        print('Could not add parameter', x[0], flush = True)
+        elif opt in ('-g', '--gains'):
+            parameters['g'] = arg
+            
+    return parameters
 
 if __name__ == '__main__':
 	params = argparser()
---
+++ code/Heterogeneity/sh/LR_no_feedbacks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#how to run
+#qsub ~/research/AutomatAnts/code/run_model.sh 
+
+cd
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+for rho in $(seq 0.001 0.1 1.001)
+do
+    for epsilon in $(seq 0.001 0.1 1.001)
+    do
+        python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/rho/LR_no_listen/ --filename rho_${rho/,/.}_epsilon_${epsilon/,/.} --social_feedbacks LR -n 100 -p "rho=${rho/,/.};epsilon=${epsilon/,/.}"
+    done
+done
