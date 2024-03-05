18bba826ca882dc690cdbcb8ea33559d0021f044
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-03-05 16:14:21 +0100

    default updated

--- code/Heterogeneity/sh/default.sh
+++ code/Heterogeneity/sh/default.sh
@@ -10,4 +10,4 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin/$LD_LIBRARY_PATH
 
 . ~/research/automatenv/bin/activate
 # python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/default/ --filename default -n 100 -m exp
-python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/default/ --filename default -n 100 -m exp -p "rho=0.5;epsilon=1"
+python3 ~/research/AutomatAnts/code/Heterogeneity/run_cluster.py --directory ~/research/AutomatAnts/results/2024/hetero_model/default/ --filename default -n 100 -p "rho=0.5;epsilon=1"
