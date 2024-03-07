4125b0cd69dc61b63c6b0e13f1a245d1015c8bae
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-03-07 10:49:59 +0100

    updated movement simulations

--- code/Movement/Model_squared_lattice/run_models.py
+++ code/Movement/Model_squared_lattice/run_models.py
@@ -4,8 +4,15 @@ from Model import *
 path = '/home/usuaris/pol.fernandez/research/AutomatAnts/results/2024/movement_results/' ## cluster
 
 for r in np.arange(0, 1.01, 0.1):
-    ## experimental scales
-    for R in np.arange(2.5, 20.01, 1.25):
+    ## small scales (experiments)
+    for R in np.arange(2.0, 20.01, 2.0):
+        m = Model(rho = r, R = R)
+        m.run()
+        fn = 'rho_%s_R_%s' % (r, R)
+        m.save_results(path, fn)
+    
+    ## large scales (hypothetical)
+    for R in np.arange(24, 101, 4):
         m = Model(rho = r, R = R)
         m.run()
         fn = 'rho_%s_R_%s' % (r, R)
