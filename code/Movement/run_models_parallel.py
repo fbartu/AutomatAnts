700c26d51f0b620226df5bfaa201fe8bd67b6218
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2024-03-07 11:58:27 +0100

    parallel execution of movement simulations

---
+++ code/Movement/run_models_parallel.py
@@ -0,0 +1,36 @@
+from Model import *
+import os, uuid, time
+from multiprocessing import Pool, cpu_count
+import itertools
+
+# path = '/home/polfer/research/gits/AutomatAnts/results/2024/movement_results/' ## local
+path = '/home/usuaris/pol.fernandez/research/AutomatAnts/results/2024/movement_simulations/' ## cluster
+        
+rho_range = np.round(np.arange(0, 1.01, 0.1), 3)
+R_range = np.round(np.arange(2.5, 20.01, 1.25), 3)
+args = list(itertools.product(rho_range, R_range))
+
+rhos = [x[0] for x in args]
+Radiuses = [x[1] for x in args]
+
+def run_model(rho, R):
+    pid = os.getpid()
+    t = int(time.time())
+    uid = uuid.uuid4().int
+    seed = hash((pid, t, uid, rho+R)) % (2**32 - 1)
+    np.random.seed(seed)
+
+    m = Model(rho = rho, R = R)
+    m.run()
+    fn = 'rho_%s_R_%s' % (rho, R)
+    m.save_results(path, fn)
+        
+        
+if __name__ == '__main__':
+
+    num_processes = cpu_count()
+    pool = Pool(processes=num_processes)
+
+    pool.starmap(run_model, [rhos, Radiuses])
+    pool.close()
+    pool.join()
