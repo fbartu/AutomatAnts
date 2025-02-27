393bd85fe88a6c36caf8e4ffda4c9616d601d806
polfe94 <pfernandez@ceab.csic.es>
2025-02-27 18:37:11 +0100

    running some more simulations

--- VERSIONS/vFixed_Density/sh/random_init_pos.sh
+++ VERSIONS/vFixed_Density/sh/random_init_pos.sh
@@ -13,7 +13,7 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin:$LD_LIBRARY_PATH
 for rho in $(seq 0.001 0.1 1.001)
 do
     float_rho=$(echo $rho | tr ',' '.')
-    for N in 5 10 20 30 40 50
+    for N in 1240 1860 2480 3100
     # for N in 5 10 20 30 40 50 75 100 150 200 400 620
     do
         # python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_cluster.py --directory ~/research/AutomatAnts/results/2025/agent_density/random_position/ --filename "rho_${rho}_N_${N}" -n 100 -p "rho=${rho/,/.};N=${N};init_position=random;tmax=2000"
