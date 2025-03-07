f9df5a21a2ec49e47b8c2ac916a4700195db4bd3
Pol Fernandez lopez <pfernandez@ceab.csic.es>
2025-03-07 19:04:00 +0100

    adding targeted positioning of scouts and recruits

---
+++ VERSIONS/vFixed_Density/sh/targeted_init_pos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#how to run
+#qsub ~/research/AutomatAnts/code/run_model.sh 
+
+cd
+
+export PATH=/home/soft/python-3.9.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin:$LD_LIBRARY_PATH
+
+. ~/research/automatenv/bin/activate
+
+for rho in $(seq 0.001 0.1 1.001)
+do
+    for N in 5 10 20 30 40 50 75 100 150 200 400 620
+    do
+        python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/asymmetric_positioning/ --filename "rho_${rho}_N_${N}" -n 100 -p "rho=${rho/,/.};N=${N};init_position=targeted;homing_behavior=False"
+
+    done
+done
