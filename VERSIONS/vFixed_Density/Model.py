90e71c485ca382c7749a94e544ba77b11f37179b
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2025-03-18 12:35:40 +0100

    updated option for clustered scouts

--- VERSIONS/vFixed_Density/Model.py
+++ VERSIONS/vFixed_Density/Model.py
@@ -332,19 +332,31 @@ class Model(Model):
             positions = random.choices(list(self.xy.keys()), k = self.N)
 
             if init_position == 'targeted':
-
-                if nSR > 0:
+                
+                
+                # CODE FOR RECRUITS // UNCOMMENT!
+                # if nSR > 0:
+                #     positions = np.array(positions)
+                #     nodes = np.array(list(self.xy.keys()))
+                #     x0 = random.choice(list(self.xy.values()))
+                #     maxd = 4.0
+                #     darray = np.array([dist(self.xy[i], x0) for i in self.xy])
+                #     idx = np.where(darray < maxd)[0]# np.where((darray > maxd) & (darray < maxd))[0]
+                #     clustered_indices = np.random.choice(idx, size = nSR, replace = True)
+                #     positions[indices] = [tuple(x) for x in nodes[clustered_indices]]
+                #     positions = [tuple(x) for x in positions]
+                #     positions[0] = random.choice(list(self.xy.keys()))
+                if nLR > 0:
                     positions = np.array(positions)
                     nodes = np.array(list(self.xy.keys()))
                     x0 = random.choice(list(self.xy.values()))
                     maxd = 4.0
                     darray = np.array([dist(self.xy[i], x0) for i in self.xy])
                     idx = np.where(darray < maxd)[0]# np.where((darray > maxd) & (darray < maxd))[0]
-                    clustered_indices = np.random.choice(idx, size = nSR, replace = True)
-                    positions[indices] = [tuple(x) for x in nodes[clustered_indices]]
+                    clustered_indices = np.random.choice(idx, size = nLR, replace = True)
+                    positions[mask] = [tuple(x) for x in nodes[clustered_indices]]
                     positions = [tuple(x) for x in positions]
                     positions[0] = random.choice(list(self.xy.keys()))
-
 
         for i in range((self.N-1), -1, -1):
             self.agents[i] = Ant(i, self, g=g[i], social=rec[i], mot_matrix=self.matrices[behav[i]], behavior = behav[i],
