be767852f008f2297a404a069cc71e9730f95b40
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2025-03-18 12:40:10 +0100

    radius = 2

--- VERSIONS/vFixed_Density/sh/targeted_init_pos.sh
+++ VERSIONS/vFixed_Density/sh/targeted_init_pos.sh
@@ -14,10 +14,19 @@ for rho in $(seq 0.001 0.1 1.001)
 do
     for N in 5 10 20 30 40 50 75 100 150 200 400 620
     do
+        # --- MAX DISTANCE = 4 --- #
         # with clustered recruits
         # python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/asymmetric_positioning/ --filename "rho_${rho}_N_${N}" -n 100 -p "rho=${rho/,/.};N=${N};init_position=targeted;homing_behavior=False"
         # with clustered scouts --> change in code
         python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/asymmetric_positioning/ --filename "rho_${rho}_N_${N}_scouts" -n 100 -p "rho=${rho/,/.};N=${N};init_position=targeted;homing_behavior=False"
+
+
+        # --- MAX DISTANCE = 2 --- # 
+        # with clustered recruits
+        python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/asymmetric_positioning/ --filename "rho_${rho}_N_${N}_R2" -n 100 -p "rho=${rho/,/.};N=${N};init_position=targeted;homing_behavior=False"
+        # with clustered scouts --> change in code
+        # python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/asymmetric_positioning/ --filename "rho_${rho}_N_${N}_R2_scouts" -n 100 -p "rho=${rho/,/.};N=${N};init_position=targeted;homing_behavior=False"
+
 
     done
 done
