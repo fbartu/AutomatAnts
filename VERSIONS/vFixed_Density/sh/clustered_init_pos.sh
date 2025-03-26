a2312fcead83d3104a289659129fae0a30b02b00
pfernandez@ceab.csic.es <pfernandez@ceab.csic.es>
2025-03-26 09:47:07 +0100

    updated clustered init

--- VERSIONS/vFixed_Density/sh/clustered_init_pos.sh
+++ VERSIONS/vFixed_Density/sh/clustered_init_pos.sh
@@ -12,7 +12,7 @@ export LD_LIBRARY_PATH=/home/soft/python-3.9.5/bin:$LD_LIBRARY_PATH
 
 for rho in $(seq 0.001 0.1 1.001)
 do
-    for N in 5 10 20 30 40 50 75 100 150
+    for N in 5 10 20 30 40 50 75 100 150 200 400 620
     do
         python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/symmetric_positioning/ --filename "rho_${rho}_N_${N}" -n 100 -p "rho=${rho/,/.};N=${N};init_position=clustered;homing_behavior=False"
 
