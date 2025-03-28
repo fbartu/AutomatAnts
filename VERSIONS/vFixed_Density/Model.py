8e9ca472007c1cdd81d413131fe29826327c90be
Pol Fernandez lopez <pfernandez@ceab.csic.es>
2025-03-28 12:37:43 +0100

    still debugging

--- VERSIONS/vFixed_Density/Model.py
+++ VERSIONS/vFixed_Density/Model.py
@@ -347,7 +347,7 @@ class Model(Model):
                     print('R is in NOT in args, setting R to ', R)
 
                 # if not 'agg_scouts' in kwargs or not eval(kwargs['agg_scouts']):
-                print(eval(kwargs['agg_scouts']), flush = True)
+                print(kwargs['agg_scouts'], flush = True)
                 if not 'agg_scouts' in kwargs or not eval(kwargs['agg_scouts']):
                     print('Aggregating recruits', flush=True)
                     # CODE FOR RECRUITS
