f781bf5a23e7cd626e9a91c807c54dd901cda0fe
Pol Fernandez lopez <pfernandez@ceab.csic.es>
2025-04-18 08:46:42 +0200

    minor bug correction in aggregated case

--- VERSIONS/vFixed_Density/sh/second_batch_scan.sh
+++ VERSIONS/vFixed_Density/sh/second_batch_scan.sh
@@ -14,9 +14,9 @@ for rho in 0.25 0.75
 do
     for N in 20 60
     do
-        python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/second_batch/scan/ --filename "homing_rho_${rho}_N_${N}" -n 100 -p "rho=${rho};N=${N};init_position=nest;homing_behavior=True"
+        # python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/second_batch/scan/ --filename "homing_rho_${rho}_N_${N}" -n 100 -p "rho=${rho};N=${N};init_position=nest;homing_behavior=True"
         python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/second_batch/scan/ --filename "agg_rho_${rho}_N_${N}" -n 100 -p "rho=${rho};N=${N};init_position=targeted;homing_behavior=False"
-        python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/second_batch/scan/ --filename "random_rho_${rho}_N_${N}" -n 100 -p "rho=${rho};N=${N};init_position=random;homing_behavior=False"
+        # python3 ~/research/AutomatAnts/VERSIONS/vFixed_Density/run_until.py --directory ~/research/AutomatAnts/results/2025/agent_density/second_batch/scan/ --filename "random_rho_${rho}_N_${N}" -n 100 -p "rho=${rho};N=${N};init_position=random;homing_behavior=False"
     done
 done
 
